bee6a8331624a79acbf71828eb8263998a7d4850
Leander Neiss <leander.neiss@student.hpi.uni-potsdam.de>
2019-06-10 21:04:26 +0200

    Add PCM functionality, register for events, record data, add results to JSON.

--- pcm_plugin.cpp
+++ pcm_plugin.cpp
@@ -1,20 +1,87 @@
 #include "pcm_plugin.hpp"
 
-#include "storage/table.hpp"
+#include "benchmark_runner.hpp"
 
 namespace opossum {
+
+PcmPlugin::PcmPlugin() :
+  _pcm(nullptr),
+  _number_of_sockets(1),
+  _links_per_socket(1),
+  _system_counter_state_before(),
+  _system_counter_state_after(),
+  _results(nlohmann::json{{"benchmarks", nlohmann::json::array()}}){}
 
 const std::string PcmPlugin::description() const { return "This is the Hyrise PcmPlugin"; }
 
 void PcmPlugin::start() {
-  TableColumnDefinitions column_definitions;
-  column_definitions.emplace_back("col_1", DataType::Int);
-  auto table = std::make_shared<Table>(column_definitions, TableType::Data);
-
-  sm.add_table("DummyTable", table);
+  std::cout << "Initializing PCM lib" << std::endl;
+  _pcm = PCM::getInstance();
+  PCM::ErrorCode returnResult = _pcm->program();
+  if (returnResult != PCM::Success){
+    std::cerr << "PCM couldn't start" << std::endl;
+    std::cerr << "Error code: " << returnResult << std::endl;
+    /*_pcm->resetPMU();*/
+    exit(1);
+  }
+  _number_of_sockets = _pcm->getNumSockets();
+  _links_per_socket = _pcm->getQPILinksPerSocket();
 }
 
-void PcmPlugin::stop() { StorageManager::get().drop_table("DummyTable"); }
+void PcmPlugin::stop() {
+  std::cout << "Cleaning up PCM lib" << std::endl;
+  _pcm->cleanup();
+}
+
+void PcmPlugin::register_listenable(std::shared_ptr<Listenable> listenable) {
+  listenable->add_listener(Event::ItemRunStarted, [&](const nlohmann::json &payload){this->_start_counting(payload);});
+  listenable->add_listener(Event::ItemRunFinished, [&](const nlohmann::json &payload){this->_stop_counting(payload);});
+  listenable->add_listener(Event::CreateReport, [&](const nlohmann::json &payload){
+    auto benchmark_runner = std::dynamic_pointer_cast<BenchmarkRunner>(listenable);
+    Assert(benchmark_runner, "Can register PcmPlugin only with BenchmarkRunner as Listenable.");
+    benchmark_runner->add_to_json_report("PcmPlugin", _results);
+  });
+}
+
+void PcmPlugin::_start_counting(const nlohmann::json &payload) {
+  _current_item_id = payload["item_id"];
+  _system_counter_state_before = getSystemCounterState();
+}
+
+void PcmPlugin::_stop_counting(const nlohmann::json &payload) {
+  _system_counter_state_after = getSystemCounterState();
+
+  Assert(payload["item_id"] == _current_item_id, "Whoopsie");
+
+  auto result = _get_result(_system_counter_state_before, _system_counter_state_after);
+
+  auto result_json = nlohmann::json{{"name", payload["item_name"]}};
+  result_json.push_back({"qpi_to_mc_traffic_ratio", result.qpi_to_mc_traffic_ratio});
+  result_json.push_back({"bytes_read_from_mc", result.bytes_read_from_mc});
+  result_json.push_back({"qpi_link_utilization_in", result.qpi_link_utilization_in});
+  result_json.push_back({"qpi_link_utilization_out", result.qpi_link_utilization_out});
+  _results["benchmarks"].push_back(result_json);
+}
+
+PcmResult PcmPlugin::_get_result(SystemCounterState before, SystemCounterState after) {
+  auto result = PcmResult();
+  for (auto socket = uint32_t{0}; socket < _number_of_sockets; ++socket) {
+    auto qpi_link_utilization_in = std::vector<double>{};
+    auto qpi_link_utilization_out = std::vector<double>{};
+
+    for (auto link = uint32_t{0}; link < _links_per_socket; ++link) {
+      qpi_link_utilization_in.push_back(getIncomingQPILinkUtilization(socket, link, before, after));
+      qpi_link_utilization_out.push_back(getOutgoingQPILinkUtilization(socket, link, before, after));
+    }
+
+    result.qpi_link_utilization_in.push_back(qpi_link_utilization_in);
+    result.qpi_link_utilization_out.push_back(qpi_link_utilization_out);
+  }
+
+  result.qpi_to_mc_traffic_ratio = getQPItoMCTrafficRatio(before, after);
+  result.bytes_read_from_mc = getBytesReadFromMC(before, after);
+  return result;
+}
 
 EXPORT_PLUGIN(PcmPlugin)
 
--- pcm_plugin.hpp
+++ pcm_plugin.hpp
@@ -1,14 +1,24 @@
 #pragma once
 
-#include "storage/storage_manager.hpp"
+#include <json.hpp>
+
 #include "utils/abstract_plugin.hpp"
 #include "utils/singleton.hpp"
 
+#include "cpucounters.h"
+
 namespace opossum {
+
+struct PcmResult {
+  double qpi_to_mc_traffic_ratio = 0.0;
+  uint64_t bytes_read_from_mc = 0;
+  std::vector<std::vector<double>> qpi_link_utilization_in = {};
+  std::vector<std::vector<double>> qpi_link_utilization_out = {};
+};
 
 class PcmPlugin : public AbstractPlugin, public Singleton<PcmPlugin> {
  public:
-  PcmPlugin() : sm(StorageManager::get()) {}
+  PcmPlugin();
 
   const std::string description() const final;
 
@@ -16,7 +26,24 @@ class PcmPlugin : public AbstractPlugin, public Singleton<PcmPlugin> {
 
   void stop() final;
 
-  StorageManager& sm;
+  void register_listenable(std::shared_ptr<Listenable> listenable);
+
+ private:
+
+  void _start_counting(const nlohmann::json &payload);
+
+  void _stop_counting(const nlohmann::json &payload);
+
+  PcmResult _get_result(SystemCounterState before, SystemCounterState after);
+
+ private:
+  PCM * _pcm;
+  uint32_t _number_of_sockets;
+  uint64_t _links_per_socket;
+  SystemCounterState _system_counter_state_before;
+  SystemCounterState _system_counter_state_after;
+  nlohmann::json _results;
+  size_t _current_item_id;
 };
 
 }  // namespace opossum
